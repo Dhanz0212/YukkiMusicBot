2189d85a513f4dcb3df6ec4a03d61ebff2e8e7b4
ʙᴧɢᴧsҡᴧʀᴧ-ᴇx <93908715+ybgskr12@users.noreply.github.com>
2022-02-01 00:59:20 +0700

    Update Ping.py

--- Yukki/Plugins/Ping.py
+++ Yukki/Plugins/Ping.py
@@ -33,7 +33,7 @@ Disk: {disk}%"""
 async def ping(_, message):
     start = datetime.now()
     response = await message.reply_photo(
-        photo="Utils/Query.jpg",
+        photo="https://telegra.ph/file/63267910585608a6302b0.jpg",
         caption=">> Pong!",
     )
     uptime = await bot_sys_stats()
